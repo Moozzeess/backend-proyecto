6ef3d21f03d706fbeff74187e10c7a3fc22ec6b2
Moozzeess <136218873+Moozzeess@users.noreply.github.com>
2026-06-26 11:22:38 -0600

    lectura de backend

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -9,6 +9,6 @@
 declare const process: any;
 
 export const entorno = {
-  produccion: false,
-  urlBaseApi: process.env['URL_BASE_API']
+  produccion: process.env.NODE_ENV === 'production',
+  urlBaseApi: process.env.NG_APP_URL_BASE_API || ''
 };
