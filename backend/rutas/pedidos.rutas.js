cd3dfc329fae4dfda77f0b879e2340e8cfdd6269
Moozzeess <136218873+Moozzeess@users.noreply.github.com>
2026-06-26 14:12:22 -0600

    token

--- backend/rutas/pedidos.rutas.js
+++ backend/rutas/pedidos.rutas.js
@@ -10,7 +10,7 @@ router.post('/', verificarToken, permitirRoles('administrador', 'empleado', 'cli
 router.get('/', verificarToken, permitirRoles('administrador', 'empleado'), PedidosControlador.obtenerTodos);
 router.patch('/:idPedido/estado', verificarToken, permitirRoles('administrador', 'empleado'), PedidosControlador.actualizarEstado);
 router.get('/historial/:idCliente', verificarToken, verificarPropietarioOAdmin('idCliente'), PedidosControlador.obtenerHistorial);
-router.post('/:idPedido/reenviar-correo', verificarToken, permitirRoles('administrador', 'empleado'), PedidosControlador.reenviarCorreo);
+router.post('/:idPedido/reenviar-correo', verificarToken, permitirRoles('administrador', 'empleado', 'cliente'), PedidosControlador.reenviarCorreo);
 router.get('/estadisticas/ventas', verificarToken, permitirRoles('administrador'), PedidosControlador.obtenerEstadisticasVentas);
 router.get('/estadisticas/productos', verificarToken, permitirRoles('administrador'), PedidosControlador.obtenerEstadisticasProductos);
 router.get('/kpis', verificarToken, permitirRoles('administrador'), PedidosControlador.obtenerKpis);
